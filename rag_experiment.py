962dd70cdc9e63d7712a29308a8f8a469e684aad
parriissaa <parisa.kaghazgaran@gmail.com>
2025-07-10 01:03:51 +0000

    Add initial RAG experiment

---
+++ rag_experiment.py
@@ -0,0 +1,8 @@
+
+# Example RAG stub using LangChain
+from langchain.document_loaders import WebBaseLoader
+from langchain.embeddings import OpenAIEmbeddings
+from langchain.vectorstores import FAISS
+
+# Placeholder for RAG pipeline
+print("RAG Pipeline Here")
